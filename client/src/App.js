46752a1a841d97c9d52d8a8cc689f30e849659af
Lindsay Deaton <lindsay-deaton@lambdastudents.com>
2021-01-27 21:21:37 -0500

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 import axios from 'axios';
+import UpdateForm from "./Movies/UpdateForm";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -31,9 +32,15 @@ const App = () => {
       <Route exact path="/">
         <MovieList movies={movieList} />
       </Route>
-
+      <Route path="/update-movie/:id" render={
+        props => {
+          return (<UpdateForm {...props} setMovieList={setMovieList} movieList={movieList}
+          />);
+        }
+      }/>
+          
       <Route path="/movies/:id">
-        <Movie addToSavedList={addToSavedList} />
+        <Movie addToSavedList={addToSavedList} setMovieList={setMovieList} />
       </Route>
     </>
   );
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,  } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
-function Movie({ addToSavedList }) {
+function Movie({ addToSavedList, setMovieList, movieList }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
+  const { push } = useHistory()
 
   const fetchMovie = (id) => {
     axios
@@ -18,6 +19,27 @@ function Movie({ addToSavedList }) {
     addToSavedList(movie);
   };
 
+  const updateMovie = () => {
+    push(`/update-movie/${params.id}`)
+  }
+
+  const deleteMovie = (id) => {
+    axios.delete(`http://localhost:5000/api/movies/${id}`)
+      .then(res => {
+        console.log(res.data)
+        const movies = [...movieList];
+        setMovieList(movies.filter(movie => {
+          return movie.id !== res.data ?
+            //params.id is checking the url
+            setMovieList(movie) : ""
+        }))
+          push('/movies')
+      })
+      .catch(err => {
+      console.log(err)
+    })
+  }
+
   useEffect(() => {
     fetchMovie(params.id);
   }, [params.id]);
@@ -29,10 +51,16 @@ function Movie({ addToSavedList }) {
   return (
     <div className="save-wrapper">
       <MovieCard movie={movie} />
-
+<button onClick={updateMovie}>
+        Update
+      </button>
+      <button onClick={deleteMovie}>
+        Delete
+      </button>
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+      
     </div>
   );
 }
